edb2b2caa921aa97bc2e162f6d5a0f0ec467fa98
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-06-09 15:47:45 +0200

    first commit

---
+++ exercice4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 4</title>
+</head>
+<body>
+    <?php
+        $phrase = "Coucou toi !";
+        $number = 12;
+        $float = 12.5;
+        $bool = true;
+
+        echo("$phrase <br> $number <br> $float <br> $bool");
+    ?>
+    
+</body>
+</html>
